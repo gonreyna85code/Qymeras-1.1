e303fc3ddea718a55470a5df5d5005b313d6431e
Gonzalo Reyna <gonreyna85@gmail.com>
2026-05-18 14:45:49 -0300

    chore: add platform compatibility layer to config.h for ESP8266/ESP32 variants

--- config.h
+++ config.h
@@ -2,12 +2,70 @@
 #include <Arduino.h>
 
 /* =========================
+   PLATAFORMA & COMPATIBILIDAD
+   ========================= */
+
+// Auto-detección
+#if defined(ESP8266)
+  #define PLATFORM_ESP8266 1
+  #include <ESP8266WiFi.h>
+  #include <ESP8266WebServer.h>
+  typedef ESP8266WebServer WebServerCompat;
+  #define ICACHE_FLASH ICACHE_FLASH_ATTR
+  #define GET_CHIP_ID() ESP.getChipId()
+  #define SET_WIFI_SLEEP() WiFi.setSleepMode(WIFI_NONE_SLEEP)
+  #define RESET_MCU() ESP.reset()
+
+#elif defined(ESP32)
+  #define PLATFORM_ESP32 1
+  #include <WiFi.h>
+  #include <WebServer.h>
+  typedef WebServer WebServerCompat;
+  #define ICACHE_FLASH IRAM_ATTR
+  #define GET_CHIP_ID() ((uint32_t)ESP.getEfuseMac())
+  #define SET_WIFI_SLEEP() WiFi.setSleep(false)
+  #define RESET_MCU() ESP.restart()
+
+#elif defined(ESP32S2)
+  #define PLATFORM_ESP32S2 1
+  #include <WiFi.h>
+  #include <WebServer.h>
+  typedef WebServer WebServerCompat;
+  #define ICACHE_FLASH IRAM_ATTR
+  #define GET_CHIP_ID() ((uint32_t)ESP.getEfuseMac())
+  #define SET_WIFI_SLEEP() WiFi.setSleep(false)
+  #define RESET_MCU() ESP.restart()
+
+#elif defined(ESP32S3)
+  #define PLATFORM_ESP32S3 1
+  #include <WiFi.h>
+  #include <WebServer.h>
+  typedef WebServer WebServerCompat;
+  #define ICACHE_FLASH IRAM_ATTR
+  #define GET_CHIP_ID() ((uint32_t)ESP.getEfuseMac())
+  #define SET_WIFI_SLEEP() WiFi.setSleep(false)
+  #define RESET_MCU() ESP.restart()
+
+#elif defined(ESP32C3)
+  #define PLATFORM_ESP32C3 1
+  #include <WiFi.h>
+  #include <WebServer.h>
+  typedef WebServer WebServerCompat;
+  #define ICACHE_FLASH IRAM_ATTR
+  #define GET_CHIP_ID() ((uint32_t)ESP.getEfuseMac())
+  #define SET_WIFI_SLEEP() WiFi.setSleep(false)
+  #define RESET_MCU() ESP.restart()
+
+#else
+  #error "Plataforma no soportada. Usa: ESP8266, ESP32, ESP32S2, ESP32S3, ESP32C3"
+#endif
+
+/* =========================
    LIMITES DEL SISTEMA
    ========================= */
 
 #define MAX_SENSORS 64
 #define MAX_RULES   20
-
 
 /* =========================
    EEPROM
@@ -35,7 +93,6 @@
 #define EEPROM_RULES_START (EEPROM_CALIB_START + EEPROM_CALIB_SIZE)
 #define EEPROM_RULES_SIZE 1600
 
-
 /* =========================
    PROTECCION EEPROM
    ========================= */
@@ -43,7 +100,6 @@
 #if (EEPROM_RULES_START + EEPROM_RULES_SIZE) > EEPROM_SIZE
 #error EEPROM layout overflow
 #endif
-
 
 /* =========================
    RED
