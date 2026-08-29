90022c6857a21b42fab870773ea939db326ea012
Gonzalo Reyna <gonreyna85@gmail.com>
2026-08-29 04:57:59 -0300

    fix(gui): fix actuator/sensor count, TIME excluded from both counts

--- src/html.cpp
+++ src/html.cpp
@@ -814,10 +814,10 @@ async function loadDevices() {
     visualSort(data).forEach((s, i) => {
       const card = deviceCard(s.name, s.value, s.id, s.state, s.fade, s.type, s);
       mobile += card;
-      if (s.type === SensorType.TYPE_RELAY || s.type === SensorType.TYPE_DIMMER || s.type === SensorType.SENSOR_TIME) {
+      if (s.type === SensorType.TYPE_RELAY || s.type === SensorType.TYPE_DIMMER) {
         actuators += card;
         actCount++;
-      } else {
+      } else if (s.type !== SensorType.SENSOR_TIME) {
         deviceSensors += card;
         sensCount++;
       }
